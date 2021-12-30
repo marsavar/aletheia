8dee0c70736239adbc2a0cef9a324428ed3092f3
Mario Savarese <mario.savarese@theguardian.com>
2021-12-30 02:33:32 +0100

    Use builder pattern, add enums for better safety.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,41 @@
-use reqwest::header::{HeaderMap, HeaderValue, InvalidHeaderValue};
-use reqwest::{Client, RequestBuilder};
+use chrono::Utc;
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use std::borrow::BorrowMut;
-use std::cell::{Cell, Ref, RefCell};
 use std::collections::HashMap;
 use std::error::Error;
-use chrono::{DateTime, FixedOffset, ParseResult, Utc};
+use std::string::ToString;
+use strum_macros::Display;
+
+#[derive(Display, Debug)]
+pub enum Field {
+    #[strum(serialize = "trailText")]
+    TrailText,
+    #[strum(serialize = "headLine")]
+    Headline,
+    #[strum(serialize = "showInRelatedContent")]
+    ShowInRelatedContent,
+    #[strum(serialize = "body")]
+    Body,
+    #[strum(serialize = "lastModified")]
+    LastModified,
+    #[strum(serialize = "hasStoryPackage")]
+    HasStoryPackage,
+    #[strum(serialize = "score")]
+    Score,
+    #[strum(serialize = "standFirst")]
+    StandFirst,
+    #[strum(serialize = "shortUrl")]
+    ShortUrl,
+    #[strum(serialize = "byline")]
+    Byline,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Response {
-    pub response: SearchResponse,
+    pub message: Option<String>,
+    pub response: Option<SearchResponse>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -29,6 +54,16 @@ pub struct SearchResponse {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
+pub struct Fields {
+    pub byline: Option<String>,
+    pub short_url: Option<String>,
+    pub trail_text: Option<String>,
+    pub headline: Option<String>,
+    pub body: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct SearchResult {
     pub id: String,
     pub r#type: String,
@@ -41,17 +76,23 @@ pub struct SearchResult {
     pub is_hosted: bool,
     pub pillar_id: Option<String>,
     pub pillar_name: Option<String>,
+    pub fields: Option<Fields>,
 }
 
+#[derive(Debug)]
 pub struct GuardianContentClient {
     http_client: reqwest::Client,
-    pub base_url: &'static str,
+    pub base_url: String,
     api_key: String,
+    pub request: HashMap<String, String>,
 }
 
 fn get_headers(client: &GuardianContentClient) -> HeaderMap {
     let mut headers = HeaderMap::new();
-    headers.insert("api-key", HeaderValue::from_str(&client.api_key[..]).unwrap());
+    headers.insert(
+        "api-key",
+        HeaderValue::from_str(&client.api_key[..]).unwrap(),
+    );
     headers
 }
 
@@ -59,23 +100,57 @@ impl GuardianContentClient {
     pub fn new(api_key: &str) -> Result<GuardianContentClient, Box<dyn Error>> {
         let client = Self {
             http_client: Client::new(),
-            base_url: "https://content.guardianapis.com",
+            base_url: String::from("https://content.guardianapis.com"),
             api_key: String::from(api_key),
+            request: HashMap::new(),
         };
         Ok(client)
     }
 
-    pub async fn search(&self, q: &str) -> Result<Vec<SearchResult>, Box<dyn Error>> {
+    pub fn search(&mut self, q: &str) -> &mut GuardianContentClient {
+        self.request.insert(String::from("q"), String::from(q));
+        self
+    }
+
+    pub fn show_fields(&mut self, show_fields: Vec<Field>) -> &mut GuardianContentClient {
+        self.request.insert(
+            String::from("show-fields"),
+            String::from(
+                show_fields
+                    .iter()
+                    .map(|x| x.to_string())
+                    .collect::<Vec<String>>()
+                    .join(","),
+            ),
+        );
+        self
+    }
+
+    pub async fn send(&mut self) -> Result<SearchResponse, Box<dyn Error>> {
+        let queries = Vec::from_iter(self.request.iter());
         let search = self
             .http_client
             .get(format!("{}/search", self.base_url))
             .headers(get_headers(&self))
-            .query(&[("q", q)])
+            .query(&queries)
             .send()
             .await?
             .json::<Response>()
             .await?;
-        Ok(search.response.results)
-    }
 
+        match search {
+            Response {
+                message: None,
+                response: None,
+            } => panic!("No response from the server."),
+            Response {
+                message: Some(x),
+                response: _,
+            } => panic!("{:?}", x),
+            Response {
+                message: _,
+                response: Some(x),
+            } => Ok(x),
+        }
+    }
 }
